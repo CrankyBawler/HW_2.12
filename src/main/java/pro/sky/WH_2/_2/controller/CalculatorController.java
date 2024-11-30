c9e1bc5cb99f9c9fec3dbd8cd29301dd6541b2c5
Cranky Bawler <6666lenia6666@mail.ru>
2024-11-30 14:29:24 +0300

    1

--- src/main/java/pro/sky/WH_2/_2/controller/CalculatorController.java
+++ src/main/java/pro/sky/WH_2/_2/controller/CalculatorController.java
@@ -1,4 +1,17 @@
 package pro.sky.WH_2._2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
+
+    @GetMapping
+    public String welcome() {
+        return ("Добро пожаловать");
+
+    }
+
 }
